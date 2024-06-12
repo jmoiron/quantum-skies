812a8b3590e392c222a7cbb2adab47cac65a085c
Jason Moiron <jmoiron@jmoiron.net>
2024-06-11 22:04:01 -0400

    fix ammonium nitrate and calcium fluoride, add t2 rocket questline

--- pack/kubejs/server_scripts/gtec.js
+++ pack/kubejs/server_scripts/gtec.js
@@ -3,6 +3,8 @@ priority: 0
 ServerEvents.recipes(event => {
     // gregtech extended chemistry support
 
+    let greg = event.recipes.gtceu;
+
     let sludgeChem = [
         ["gtceu:purified_pentlandite_ore", 2, "gtceu:sulfuric_nickel_solution"],
         ["gtceu:purified_tetrahedrite_ore", 2, "gtceu:sulfuric_copper_solution"],
@@ -14,7 +16,7 @@ ServerEvents.recipes(event => {
 
     event.remove({type: "gtceu:chemical_bath", input: "gtceu:crushed_cooperite_ore"});
 
-    event.recipes.gtceu.chemical_bath("cooperite_mercury_modified")
+    greg.chemical_bath("cooperite_mercury_modified")
         .itemInputs("gtceu:crushed_cooperite_ore")
         .inputFluids("gtceu:mercury 100")
         .itemOutputs("gtceu:purified_cooperite_ore")
@@ -31,7 +33,7 @@ ServerEvents.recipes(event => {
     // also, nerf as a source of titanium
     event.remove({id: "gtceu:electrolyzer/decomposition_electrolyzing_monazite"});
 
-    event.recipes.gtceu.electrolyzer("decomposition_electrolyzing_monazite")
+    greg.electrolyzer("decomposition_electrolyzing_monazite")
         .inputFluids("gtec:diluted_monazite_rare_earth_mud 1000")
         .chancedOutput("gtec:monazite_sulfate_dust", 9000, 10)
         .chancedOutput("gtceu:silicon_dioxide_dust", 7500, 10)
@@ -42,19 +44,43 @@ ServerEvents.recipes(event => {
         .duration(200);
 
     // add missing recipe for ammonium nitrate solution
-    event.recipes.gtceu.chemical_reactor("ammonium_nitrate")
+    greg.chemical_reactor("ammonium_nitrate")
         .inputFluids("gtceu:ammonia 1000")
         .inputFluids("gtceu:nitric_acid 1000")
         .outputFluids("gtec:ammonium_nitrate_solution 1000")
         .EUt(400)
         .duration(30);
 
-    event.recipes.gtceu.electrolyzer("decomposition_electrolyzing_ammonium_nitrate")
+    greg.electrolyzer("decomposition_electrolyzing_ammonium_nitrate")
         .inputFluids("gtec:ammonium_nitrate_solution 1000")
         .outputFluids("gtceu:ammonia 1000")
         .outputFluids("gtceu:nitric_acid 1000")
         .EUt(480)
         .duration(600);
+
+    event.remove({id: "gtceu:electrolyzer/decomposition_electrolyzing_calcium_fluoride"});
+
+    greg.electrolyzer("decomposition_calcium_fluoride_fixed")
+        .itemInputs("3x gtec:calcium_fluoride_dust")
+        .itemOutputs("gtceu:calcium_dust")
+        .outputFluids("gtceu:fluorine 2000")
+        .EUt(30)
+        .duration(72);
+
+    greg.chemical_reactor("calcium_fluoride_assembly")
+        .itemInputs("gtceu:calcium_dust")
+        .inputFluids("gtceu:fluorine 2000")
+        .itemOutputs("3x gtec:calcium_fluoride_dust")
+        .EUt(120)
+        .duration(120);
+
+    event.remove({id: "gtceu:extractor/extract_calcium_fluoride_dust"});
+
+    greg.extractor("extract_calcium_fluoride_dust")
+        .itemInputs("gtec:calcium_fluoride_dust")
+        .outputFluids("gtec:calcium_fluoride 1000")
+        .EUt(30)
+        .duration(24);
 
     // vanilla GC yields vs GTEC yields:
 
