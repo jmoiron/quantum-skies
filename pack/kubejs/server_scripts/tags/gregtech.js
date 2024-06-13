fe046ce196466aaac33dd90ad589c66bdbf51651
Jason Moiron <jmoiron@jmoiron.net>
2024-06-13 13:58:51 -0400

    remove planks tag from treated wood

--- pack/kubejs/server_scripts/tags/gregtech.js
+++ pack/kubejs/server_scripts/tags/gregtech.js
@@ -4,4 +4,8 @@ ServerEvents.tags("item", (event) => {
   event.add("ad_astra:fuel", "gtceu:rp_1_rocket_fuel");
   event.add("ad_astra:fuel", "gtceu:udmh_rocket_fuel");
   event.add("ad_astra:fuel", "gtceu:methylhydrazine_nitrate_rocket_fuel");
+
+  // remove planks from treated planks, which is getting this tag
+  // from an unknown source
+  event.remove("minecraft:planks", "gtceu:treated_wood_planks");
 });
