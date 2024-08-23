21171ed39d54ff5c50318bb7d5ba9f62c734b837
Jason Moiron <jmoiron@jmoiron.net>
2024-08-23 09:28:35 -0400

    switch to gtec-ex, remove kjs fixes that were backported into gtec

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # adapted from public domain software from Merith-TK/modpack-template
 
-VERSION := 0.2.0
+VERSION := 0.2.1
 #WINUSER := jmoir
 WINUSER := jlmoi
 PACKNAME := Quantum-Skies
--- pack/kubejs/server_scripts/gtec.js
+++ pack/kubejs/server_scripts/gtec.js
@@ -14,21 +14,6 @@ ServerEvents.recipes(event => {
         // sheldonite is disabled by gtec
     ]
 
-    event.remove({type: "gtceu:chemical_bath", input: "gtceu:crushed_cooperite_ore"});
-
-    greg.chemical_bath("cooperite_mercury_modified")
-        .itemInputs("gtceu:crushed_cooperite_ore")
-        .inputFluids("gtceu:mercury 100")
-        .itemOutputs("gtceu:purified_cooperite_ore")
-        .chancedOutput("gtec:palladium_metallic_powder_dust", 7000, 580)
-        .chancedOutput("gtceu:stone_dust", 4000, 650)
-        .EUt(30)
-        .duration(200);
-
-    // remove vanilla osmium line
-    event.remove({id: "gtceu:distillery/acidic_osmium_solution_separation_to_hydrochloric_acid"});
-    event.remove({id: "gtceu:distillery/acidic_osmium_solution_separation_to_water"});
-
     // modify pre-moon source of titanium to be EV (post-titanium)
     // also, nerf as a source of titanium
     event.remove({id: "gtceu:electrolyzer/decomposition_electrolyzing_monazite"});
@@ -43,129 +28,11 @@ ServerEvents.recipes(event => {
         .EUt(1920)
         .duration(200);
 
-    // add missing recipe for ammonium nitrate solution
-    greg.chemical_reactor("ammonium_nitrate")
-        .inputFluids("gtceu:ammonia 1000")
-        .inputFluids("gtceu:nitric_acid 1000")
-        .outputFluids("gtec:ammonium_nitrate_solution 1000")
-        .EUt(400)
-        .duration(30);
-
-    greg.electrolyzer("decomposition_electrolyzing_ammonium_nitrate")
-        .inputFluids("gtec:ammonium_nitrate_solution 1000")
-        .outputFluids("gtceu:ammonia 1000")
-        .outputFluids("gtceu:nitric_acid 1000")
-        .EUt(480)
-        .duration(600);
-
-    event.remove({id: "gtceu:electrolyzer/decomposition_electrolyzing_calcium_fluoride"});
-
-    greg.electrolyzer("decomposition_calcium_fluoride_fixed")
-        .itemInputs("3x gtec:calcium_fluoride_dust")
-        .itemOutputs("gtceu:calcium_dust")
-        .outputFluids("gtceu:fluorine 2000")
-        .EUt(30)
-        .duration(72);
-
-    greg.chemical_reactor("calcium_fluoride_assembly")
-        .itemInputs("gtceu:calcium_dust")
-        .inputFluids("gtceu:fluorine 2000")
-        .itemOutputs("3x gtec:calcium_fluoride_dust")
-        .EUt(120)
-        .duration(120);
-
-    event.remove({id: "gtceu:extractor/extract_calcium_fluoride_dust"});
-
-    greg.extractor("extract_calcium_fluoride_dust")
-        .itemInputs("gtec:calcium_fluoride_dust")
-        .outputFluids("gtec:calcium_fluoride 1000")
-        .EUt(30)
-        .duration(24);
-
     // this recipe invalidates the rest of the rhodium line
-    event.remove({id: "gtceu:electrolyzer/rhodium_sulfate_separation"});
 
-    // rhodium rebalance 46% increase yield
-    event.remove({id: "gtec:sifter/rhodium_filter_cake"});
-
-    greg.sifter("rhodium_filter_cake")
-        .itemInputs("gtec:rhodium_nitrate_dust")
-        .chancedOutput("gtec:rhodium_filter_cake_dust", 1500, 0)
-        .chancedOutput("gtec:rhodium_filter_cake_dust", 1500, 0)
-        .chancedOutput("gtec:rhodium_filter_cake_dust", 1500, 0)
-        .chancedOutput("gtec:rhodium_filter_cake_dust", 1500, 0)
-        .chancedOutput("gtec:rhodium_filter_cake_dust", 1500, 0)
-        .chancedOutput("gtec:rhodium_filter_cake_dust", 2000, 0)
-        .EUt(120)
-        .duration(300);
-
-    // 5x yield here
-    event.remove({id: "gtec:mixer/rhodium_salt_solution"});
-
-    greg.mixer("rhodium_salt_solution")
-        .itemInputs("gtec:rhodium_salt_dust")
-        .inputFluids("minecraft:water 1000")
-        .outputFluids("gtec:rhodium_salt_solution 1000")
-        .EUt(30)
-        .duration(30);
-
-    // platline fixes in PR #9, backported via kjs until we get a release
-    // fix tetrahedrite being 9x better yield than everything else
-    event.remove({id: "gtec:chemical_reactor/dissolve_tetrahedrite_for_platline"});
-
-    greg.chemical_reactor("dissolve_tetrahedrite_for_platinum")
-        .itemInputs("9x gtceu:purified_tetrahedrite_ore")
-        .itemInputs("9x gtec:platinum_metallic_powder_dust")
-        .inputFluids("gtceu:aqua_regia 10000")
-        .outputFluids("gtec:platinum_concentrate 10000")
-        .itemOutputs("gtec:platinum_residue_dust")
-        .EUt(30)
-        .duration(2250);
-
-    // fix sifter yields for platinum and palladium
-    event.remove({id: "gtec:sifter/salt_to_metallic_powder_palladium"});
-    greg.sifter("salt_to_metallic_powder_palladium")
-        .itemInputs("gtec:palladium_salt_dust")
-        .chancedOutput("gtec:palladium_metallic_powder_dust", 1500, 0)
-        .chancedOutput("gtec:palladium_metallic_powder_dust", 1500, 0)
-        .chancedOutput("gtec:palladium_metallic_powder_dust", 1500, 0)
-        .chancedOutput("gtec:palladium_metallic_powder_dust", 1500, 0)
-        .chancedOutput("gtec:palladium_metallic_powder_dust", 1500, 0)
-        .chancedOutput("gtec:palladium_metallic_powder_dust", 2000, 0)
-        .EUt(30)
-        .duration(600);
-
-    event.remove({id: "gtec:sifter/refined_platinum_salt"});
-    greg.sifter("salt_to_metallic_powder_platinum")
-        .itemInputs("gtec:platinum_salt_dust")
-        .chancedOutput("gtec:platinum_metallic_powder_dust", 1500, 0)
-        .chancedOutput("gtec:platinum_metallic_powder_dust", 1500, 0)
-        .chancedOutput("gtec:platinum_metallic_powder_dust", 1500, 0)
-        .chancedOutput("gtec:platinum_metallic_powder_dust", 1500, 0)
-        .chancedOutput("gtec:platinum_metallic_powder_dust", 1500, 0)
-        .chancedOutput("gtec:platinum_metallic_powder_dust", 2000, 0)
-        .EUt(30)
-        .duration(600);
-
-    event.remove({id: "gtec:electric_blast_furnace/rhodium_sulfate"});
-    greg.electric_blast_furnace("rhodium_sulfate")
-        .itemInputs("gtec:platinum_residue_dust")
-        .itemInputs("2x gtec:small_potassium_disulfate_dust")
-        .circuit(2)
-        .itemOutputs("gtec:leach_residue_dust")
-        .outputFluids("gtceu:rhodium_sulfate 360")
-        .blastFurnaceTemp(775)
-        .EUt(120)
-        .duration(200);
-
-    // add missing hexafluorosilic acid recipe
-    greg.chemical_reactor("hexafluorosilic_acid")
-        .inputFluids("gtceu:fluorine 6000")
-        .itemInputs("gtceu:silicon_dust")
-        .outputFluids("gtceu:hydrogen 4000")
-        .outputFluids("gtec:hexafluorosilic_acid 1000")
-        .EUt(120)
-        .duration(400);
+    // gtec-ex fix for sifting changes the platinum line slightly
+    // qs made salt -> metallic powder, but in gtec, salt -> refined salt -> metallic powder
+    // this will break the recycling on existing platinum chains
 
     // this creates an infinite processing loop that manifests cerium from nothing
     // adding a chance to not get the bastnasite back out, which reduces the yield
