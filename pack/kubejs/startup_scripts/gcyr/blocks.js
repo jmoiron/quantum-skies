38d70acb0d5cb1e0c32252a9b1de59b9c0190ba4
Jason Moiron <jmoiron@jmoiron.net>
2025-06-24 19:33:24 -0400

    wip non functional io (broken noise settings)

--- pack/kubejs/startup_scripts/gcyr/blocks.js
+++ pack/kubejs/startup_scripts/gcyr/blocks.js
@@ -2,9 +2,24 @@ StartupEvents.registry("block", (event) => {
 
     let tinted = function(blockName, color) {
         return event.create(blockName)
-            .color(1, color)
-            .item((builder) => { builder.color(1, color) })
+            .color(0, color)
+            .item((builder) => { builder.color(0, color) })
     }
+
+    // io
+    let blocks = {
+        "pyroclastic_regolith": "#ff0000",
+        "sulfuric_pumice": "#ddaa99",
+        "cryo_sulfur_deposit": "#0000ff",
+        "tidal_fractured_peridotite": "#00ff00",
+        "io_volcanic_ash": "#dddddd",
+        "lava_skylight_crust": "#444444",
+        "plasma_tempered_basalt": "#777777"
+    }
+
+    Object.entries(blocks).forEach(([name, color]) => {
+        tinted(name, color).textureAll("minecraft:block/stone");
+    })
 
     event.create("europa_ice_block");
     event.create("europa_packed_ice");
@@ -14,5 +29,7 @@ StartupEvents.registry("block", (event) => {
     event.create("tholin_ice_dark");
     event.create("tholin_ice_red");
     event.create("tholin_ice_block");
+
+
 
 })
