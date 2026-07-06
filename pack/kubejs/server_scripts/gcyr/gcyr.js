4b50dc89c4df710bc9f39b3d3ff8ebf41c4d8091
Jason Moiron <jmoiron@jmoiron.net>
2026-07-06 13:33:57 -0400

    fix the rock->cobble->regolith / rock->cobble->sand->regolith forge hammer chains after gcyr stone unification

--- pack/kubejs/server_scripts/gcyr/gcyr.js
+++ pack/kubejs/server_scripts/gcyr/gcyr.js
@@ -211,21 +211,19 @@ ServerEvents.recipes(event => {
 
     event.remove({type: "gtceu:macerator", output: "gtceu:moon_regolith_dust"});
 
-    hammer("moon_stone_to_sand", "gcyr:moon_stone", "gcyr:moon_sand")
+    hammer("moon_cobble_to_sand", "gcyr:moon_cobblestone", "gcyr:moon_sand")
     hammer("moon_sand_to_regolith_block", "gcyr:moon_sand", "gtceu:moon_regolith_block");
     macerate("moon_regolith_block_to_dust", "gtceu:moon_regolith_block", "4x gtceu:moon_regolith_dust");
 
     // mercury
     macerate("mercury_regolith_block_to_dust", "gtceu:mercury_regolith_block", "4x gtceu:mercury_regolith_dust")
-    hammer("mercury_rock_to_regolith", "gcyr:mercury_rock", "gtceu:mercury_regolith_block");
+    hammer("mercury_cobble_to_regolith", "gcyr:mercury_cobblestone", "gtceu:mercury_regolith_block");
 
     // break down mars rock
-    hammer("mars_rock_to_cobble", "gcyr:martian_rock", "gcyr:martian_cobblestone");
     hammer("mars_cobble_to_regolith", "gcyr:martian_cobblestone", "gcyr:mars_regolith");
     macerate("mars_regolith_to_dust", "gcyr:mars_regolith", "4x gtceu:mars_regolith_d_dust");
 
     // break down venus rock
-    hammer("venus_stone_to_cobble", "gcyr:venus_rock", "gcyr:venus_cobblestone");
     hammer("venus_cobble_to_sand", "gcyr:venus_cobblestone", "gcyr:venus_sand");
     hammer("venus_sand_to_regolith", "gcyr:venus_sand", "gcyr:venusian_regolith");
     macerate("venus_regolith_block_to_dust", "gcyr:venusian_regolith", "4x gtceu:venus_regolith_d_dust");
